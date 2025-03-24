1d7223142cd9b4ff8c8619aa006a51335ac1e221
dd.ddd <dvteixeira24@gmail.com>
2025-03-24 16:19:19 +0200

    Update .env.example path, enhance make.ps1 for build process, and set package version in package.json

--- make.ps1
+++ make.ps1
@@ -1,3 +1,7 @@
+Remove-Item -LiteralPath "dist" -Force -Recurse
+
+bun run build
+
 # Define source and destination paths
 $source = ".\.env.example"
 
@@ -8,14 +12,21 @@ else {
     Write-Host "Source file does not exist."
 }
 
-$destination = ".\dist\server\.env"
+$destination = ".\dist\.env"
 # Copy the .env file to the server directory
 Copy-Item -Path $source -Destination $destination -Force
 
+
+$packageJson = Get-Content -Path ".\package.json" | ConvertFrom-Json
+$version = $packageJson.version
 # Define the TAG value (you can set this as needed, e.g., "v1.0" or use Git tag if you prefer)
+$commitHash = git rev-parse --short HEAD
+
+# Combine the version and commit hash
+$TAG = "v$version-$commitHash"
 
 # Compress the contents of the dist directory into a zip file named $TAG.zip
-$zipFileName = ".\dist\$TAG.zip"
+$zipFileName = ".\dist\firebot-shortcutter-$TAG.zip"
 Compress-Archive -Path ".\dist\*" -DestinationPath $zipFileName
 
 Write-Host "Zipped contents of 'dist' into $zipFileName"
